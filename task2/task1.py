e6364a4db76ad408db5c6b5fe76e17fe89c3f260
HoLLsYYY <ibykov0909@mail.ru>
2025-11-19 13:10:55 +0300

    Update task1.py

--- task2/task1.py
+++ task2/task1.py
@@ -1,1 +1,11 @@
-
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+a = []
+b = []
+for row in matrix:
+    for element in row:
+        if element % 2 != 0:
+            a.append(element)
+        else:
+            b.append(element)
+print(a)
+print(len(b))
