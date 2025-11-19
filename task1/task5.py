3aa6dd5209f8edbff683cd5b2a406206559e651b
HoLLsYYY <ibykov0909@mail.ru>
2025-11-19 13:05:51 +0300

    Update task5.py

--- task1/task5.py
+++ task1/task5.py
@@ -1,1 +1,18 @@
-
+a = []
+b = []
+c = []
+print('Введите количество чисел: ')
+numbers = int(input())
+for i in range(numbers):
+    print('Введите числа:')
+    number = int(input())
+    if number > 0:
+        a.append(number)
+    if number < 0:
+        b.append(number)
+    else:
+        c.append(number)
+for_a = sum(a)/len(a)
+for_b = sum(b)
+for_c = "*" * len(c)
+print(f'{for_a}\n{for_b}\n{for_c}')
